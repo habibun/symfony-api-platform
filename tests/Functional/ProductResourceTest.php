803c3973e2c1869e35346464f25f6c46a55989e9
Habibun Noby <habibun.noby@gmail.com>
2023-03-17 02:19:28 +0600

    Chapter 13: Logging in Inside the Test

--- tests/Functional/ProductResourceTest.php
+++ tests/Functional/ProductResourceTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Functional;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
 
 class ProductResourceTest extends ApiTestCase
 {
@@ -13,6 +14,28 @@ class ProductResourceTest extends ApiTestCase
         $client->request('POST', '/api/products', [], [], [], [
 
         ]);
-        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseStatusCodeSame(401);
+
+
+        // create new user
+        $user = new User();
+        $user->setUsername('user1');
+        $user->setEmail('user1@localhost.com');
+        $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$z5LDVzV17j7VdApSLiX51A$qJN0dTwj6Shy1PedVKfU8TFPaI17cqlmscVkwKiz8OQ');
+
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        // create a login request
+        $client->request('POST', '/api/login', [
+           'json' => [
+               'email' => 'user1@localhost.com',
+               'password' => 'user1',
+           ]
+        ]);
+
+        // check status
+        $this->assertResponseStatusCodeSame(204);
     }
 }
