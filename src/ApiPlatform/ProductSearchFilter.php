2b1e56fd50d5f8d0bee4e8e07cf4c4316bc7dc1a
Habibun Noby <habibun.noby@gmail.com>
2023-04-07 15:00:39 +0600

    Chapter 26: Custom Filter Logic for Entities

--- src/ApiPlatform/ProductSearchFilter.php
+++ src/ApiPlatform/ProductSearchFilter.php
@@ -12,12 +12,29 @@ final class ProductSearchFilter extends AbstractFilter
 {
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = [])
     {
+        if ($property !== 'search') {
+            return;
+        }
 
+        $alias = $queryBuilder->getRootAliases()[0];
+        // a param name that is guaranteed unique in this query
+        $valueParameter = $queryNameGenerator->generateParameterName('search');
+        $queryBuilder->andWhere(sprintf('%s.title LIKE :%s OR %s.description LIKE :%s', $alias, $valueParameter, $alias, $valueParameter))
+            ->setParameter($valueParameter, '%'.$value.'%');
     }
 
     // This function is only used to hook in documentation generators (supported by Swagger and Hydra)
     public function getDescription(string $resourceClass): array
     {
-        dd($this->properties);
+        return [
+            'search' => [
+                'property' => null,
+                'type' => 'string',
+                'required' => false,
+                'openapi' => [
+                    'description' => 'Search across multiple fields',
+                ],
+            ]
+        ];
     }
 }
