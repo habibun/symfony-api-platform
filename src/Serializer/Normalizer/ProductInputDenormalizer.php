1527a09ac7adca4c139d5328297c9268c9d1d9bd
Habibun Noby <habibun.noby@gmail.com>
2023-04-08 03:38:34 +0600

    Chapter 41: DTO Input Initializer Logic

--- src/Serializer/Normalizer/ProductInputDenormalizer.php
+++ src/Serializer/Normalizer/ProductInputDenormalizer.php
@@ -3,16 +3,25 @@
 namespace App\Serializer\Normalizer;
 
 use App\Dto\ProductInput;
+use App\Entity\Product;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+
 class ProductInputDenormalizer implements DenormalizerInterface, CacheableSupportsMethodInterface
 {
+    private $objectNormalizer;
+    public function __construct(ObjectNormalizer $objectNormalizer)
+    {
+        $this->objectNormalizer = $objectNormalizer;
+    }
+
     public function denormalize($data, string $type, string $format = null, array $context = [])
     {
-        $dto = new ProductInput();
-        $dto->title = 'I am set in the denormalizer!';
-        $context[AbstractNormalizer::OBJECT_TO_POPULATE] = $dto;
+        $context[AbstractNormalizer::OBJECT_TO_POPULATE] = $this->createDto($context);
+        return $this->objectNormalizer->denormalize($data, $type, $format, $context);
     }
 
     public function supportsDenormalization($data, string $type, string $format = null)
@@ -23,5 +32,24 @@ class ProductInputDenormalizer implements DenormalizerInterface, CacheableSuppor
     public function hasCacheableSupportsMethod(): bool
     {
         return true;
+    }
+
+    private function createDto(array $context): ProductInput
+    {
+        $entity = $context['object_to_populate'] ?? null;
+        $dto = new ProductInput();
+        // not an edit, so just return an empty DTO
+        if (!$entity) {
+            return $dto;
+        }
+        if (!$entity instanceof Product) {
+            throw new \Exception(sprintf('Unexpected resource class "%s"', get_class($entity)));
+        }
+        $dto->title = $entity->getName();
+        $dto->price = $entity->getPrice();
+        $dto->description = $entity->getDescription();
+        $dto->owner = $entity->getManufacturer();
+        $dto->isPublished = $entity->getIsActive();
+        return $dto;
     }
 }
